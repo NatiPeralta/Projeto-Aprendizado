19b08244dc9999cfb758a770897f6bb0fb67c567
Natália Peralta <nati.g.peralta@hotmail.com>
2022-05-09 22:55:24 -0300

    Mudando o local de cada arquivo para a navegação do cadastrar

--- php/cadastrar.php
+++ php/cadastrar.php
@@ -2,25 +2,25 @@
 
 
 ?>
-<form name = "dadosAluno" action = "conexao.php" method="POST">
-    <table border="1">
-        <tbody>
-        <tr>
-            <td>Nome</td>
-            <td><input type="text" name="nome" value="" /></td>
-        </tr>
-        <tr>
-            <td>Nascimento</td>
-            <td><input type="date" name="nascimento" value="" /></td>
-        </tr>
-        <tr>
-            <td>Email</td>
-            <td><input type="text" name="email" value="" /></td>
-        </tr>
-        <tr>
-            <td><input type="hidden" name="acao" value="cadastrar" /></td>
-            <td><input type="submit" value="Cadastrar" name="Cadastrar" /></td>
-        </tr>
-        </tbody>
-    </table>
-</form>
+    <form name = "dadosAluno" action = "conexao.php" method="POST">
+        <table border="1">
+            <tbody>
+            <tr>
+                <td>Nome</td>
+                <td><input type="text" name="nome" value="" /></td>
+            </tr>
+            <tr>
+                <td>Nascimento</td>
+                <td><input type="date" name="nascimento" value="" /></td>
+            </tr>
+            <tr>
+                <td>Email</td>
+                <td><input type="text" name="email" value="" /></td>
+            </tr>
+            <tr>
+                <td><input type="hidden" name="acao" value="cadastrar" /></td>
+                <td><input type="submit" value="Cadastrar" name="Cadastrar" /></td>
+            </tr>
+            </tbody>
+        </table>
+    </form>
