1ff68e8431b7d08877f7d3a008fe179c33b61c3c
Natália Peralta <nati.g.peralta@hotmail.com>
2022-05-16 10:14:36 -0300

    Elaborando a tela de login

---
+++ php/login.php
@@ -0,0 +1,18 @@
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8"/>
+        <title>Login</title>
+        <link rel="stylesheet" type="text/css" href="./css/main-login.css">
+    </head>
+<body>
+<div id="corpo-form">
+    <h1>Entrar</h1>
+    <form method="POST" action="conexao.php">
+        <input type="email" placeholder="Usuário">
+        <input type="senha" placeholder="Senha">
+        <input type="submit" value="ACESSAR">
+        <a href="">Ainda não é inscrito? <strong>Cadastre-se!</strong></a>
+    </form>
+</div>
+</body>
+</html>
